f138f2319f48ffad0f39e57518c176a869acf0cb
SergeyBakunin <serge.bakunin@gmail.com>
2022-06-11 11:33:07 +0300

    Add project files.

---
+++ HW_8/Greeting.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_8
+{
+    internal class Greeting
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Properties.Settings.Default.Greeting);
+            try
+            {
+                Console.WriteLine(Properties.Settings.Default.Name);
+                Console.WriteLine((Properties.Settings.Default.Age).ToString());
+                Console.WriteLine(Properties.Settings.Default.Job);
+            }
+            catch
+            {
+                Console.WriteLine("Setting file doesn't exist!");
+            }
+            Console.Write("Name: ");
+            Properties.Settings.Default.Name = Console.ReadLine();
+
+            Console.Write("Age: ");
+            Properties.Settings.Default.Age = Console.ReadLine();
+                        
+            Console.Write("Job: ");
+            Properties.Settings.Default.Job = Console.ReadLine();
+            Properties.Settings.Default.Save();
+        }
+    }
+}
---
+++ HW_8/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Settings")]
+[assembly: AssemblyDescription("SettingsFile")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Sergey Bakunin inc.")]
+[assembly: AssemblyProduct("HW_8")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(true)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("032ae7f8-6a92-42fe-b8b1-e5b0690b5197")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.2.3.4")]
+[assembly: AssemblyFileVersion("1.2.3.4")]
